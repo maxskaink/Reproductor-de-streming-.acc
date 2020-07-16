249e18c8d6b2d79db405235cbaa5cd038bb5b830
maxskaink <calambasvivas11@gmail.com>
2020-07-16 15:10:02 -0500

    He comentado ciertas cosas que no me gustaron como quedaron, y me parece que quedo mejro asi. Ademas de añadir la funcionalidad de mute

--- assets/plugins/Multimedia.ts
+++ assets/plugins/Multimedia.ts
@@ -3,18 +3,31 @@ class Multimedia {
     boton_PlayOrPause: HTMLAudioElement;
     pausado: boolean;
     imagen_PlayOrPause: HTMLElement;
+    boton_MoreVolume: HTMLElement;
+    boton_LessVolume: HTMLElement;
+    boton_Mute: HTMLElement;
+    imagen_muteOrMute: HTMLElement;
+    volume_grow: number;
     constructor() {
         //Declarar variables necesarias
         this.pausado = true;
+        this.volume_grow = 0.1;
         //Traer objetos del DOM
-            //Trer botones
         this.audio = document.querySelector("#audio_en_vivo");
-        this.boton_PlayOrPause= document.querySelector("#boton_PlayOrPause");
+        this.audio.volume = 0.5;
+            //Trer botones
+            this.boton_PlayOrPause = document.querySelector("#boton_PlayOrPause");
+/*             this.boton_MoreVolume = document.querySelector("#button_mas_volume");
+            this.boton_LessVolume = document.querySelector("#button_menos_volume"); */
+            this.boton_Mute = document.querySelector("#button_mute_volume");
             //Traer imagenes
-        this.imagen_PlayOrPause = document.getElementById("PLay_Pause_image");
-        //Poner en la escucha los botones
-
-        this.boton_PlayOrPause.onclick= () => this.playOrPause();
+            this.imagen_PlayOrPause = document.getElementById("PLay_Pause_image");
+            this.imagen_muteOrMute = document.getElementById("imagen_muteOrMute");
+            //Poner en la escucha los botones
+            this.boton_PlayOrPause.onclick= () => this.playOrPause();
+/*             this.boton_MoreVolume.onclick = () => this.more_volume();
+            this.boton_LessVolume.onclick = () => this.less_volume(); */
+            this.boton_Mute.onclick = () => this.muteOrUnmute_volume();
     }
     playOrPause() {
         if (this.pausado) {
@@ -25,6 +38,27 @@ class Multimedia {
             this.audio.pause();
             this.imagen_PlayOrPause.src ="https://img.icons8.com/android/24/000000/play.png";
             this.pausado = true;
+        }
+    }
+    more_volume() {
+        if (this.audio.volume < 0.95){
+            this.audio.volume += this.volume_grow;
+        }
+    }
+    less_volume() {
+        if (this.audio.volume > 0.11){
+            this.audio.volume -= this.volume_grow;
+            console.log(this.audio.volume);
+        }else if (this.audio.volume < 0.11){
+            this.audio.volume= 0;
+        }
+    }
+    muteOrUnmute_volume() {
+        if (this.audio.volume === 0){
+            this.audio.volume = 0.5;
+        }else {
+            this.audio.volume = 0;
+            console.log(this.audio.volume);
         }
     }
 }
